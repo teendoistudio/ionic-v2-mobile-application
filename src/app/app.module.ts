7e8a5dd1a50cdebc8c9e851fc97538a33047d0b5
Sakchai Kantada <sakchaiwebmaster@gmail.com>
2017-02-03 23:51:55 +0700

    firebase uncompate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import {NgModule, ErrorHandler} from '@angular/core';
 import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import {Storage} from '@ionic/storage';
 import {MyApp} from './app.component';
-
 import {HomePage} from '../pages/home/home';
 import {BookPage} from '../pages/book/book';
 import {LocalStoragePage} from '../pages/local-storage/local-storage';
+import {AngularFireModule} from 'angularfire2';
+import * as firebase from 'firebase';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAAyVKGOiYgdWmHl4BxKeTzGYyW9U_qpYY",
+    authDomain: "ionic2-firebase-ef4f2.firebaseapp.com",
+    databaseURL: "https://ionic2-firebase-ef4f2.firebaseio.com",
+    storageBucket: "ionic2-firebase-ef4f2.appspot.com",
+    messagingSenderId: "744511720786"
+}
 
 @NgModule({
     declarations: [
@@ -15,7 +24,8 @@ import {LocalStoragePage} from '../pages/local-storage/local-storage';
         LocalStoragePage
     ],
     imports: [
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        AngularFireModule.initializeApp(firebaseConfig)
     ],
     bootstrap: [IonicApp],
     entryComponents: [
