794ba4fcf98271802e4c34053d8f220a765a025d
Sakchai Kantada <sakchaiwebmaster@gmail.com>
2017-02-05 21:23:49 +0700

    api service

--- angular2-server.php
+++ angular2-server.php
@@ -1,11 +1,15 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
 	$data = array(
-		'id' => 543341,
+		[
+		'id' => '1234',
 		'name' => 'Sakchai Kantada',
-		'age' => 30,
+		'age' => '30',
 		'email' => 'sakchaiwebmaster@gmail.com',
-		'tal' => '0832092143'
+		'tel' => '0832092143',
+		'price' => '2500',
+		'date' => '1986-05-31'
+		]
 	);
 
 	echo json_encode($data);
